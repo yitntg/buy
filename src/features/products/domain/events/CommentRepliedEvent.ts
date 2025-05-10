78a7ad688481671a4559d2e0f33fdc9a0d3cad88
Yang Tingting <yang@example.com>
2025-05-10 22:44:33 +0800

    添加: 创建 CommentRepliedEvent 类

--- src/features/products/domain/events/CommentRepliedEvent.ts
+++ src/features/products/domain/events/CommentRepliedEvent.ts
@@ -1,1 +1,16 @@
- 
+import { DomainEvent } from '@/shared/domain/events/DomainEvent';
+import { Comment } from '../Comment';
+
+export class CommentRepliedEvent extends DomainEvent {
+  constructor(
+    public readonly comment: Comment,
+    public readonly replyToId: string,
+    public readonly userId: string
+  ) {
+    super();
+  }
+
+  eventName(): string {
+    return 'comment.replied';
+  }
+} 
